6e561bbbb2e3877a5411aa60be62e23314faaa39
Yefhem <sametcelikbusiness@gmail.com>
2022-06-16 17:48:56 +0300

    main.go edit

--- main.go
+++ main.go
@@ -1,27 +1,31 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "fmt"
+
+type employee struct {
+	name      string
+	age       int
+	isMarried bool
+}
+
+type manager struct {
+	employee
+	hasDegree bool
+}
 
 func main() {
-	colors := []string{"red", "blue", "orange", "pink", "purple"}
+	employee := employee{
+		name:      "steve",
+		age:       20,
+		isMarried: true,
+	}
 
-	firstChannel := make(chan string)
+	manager := manager{
+		employee:  employee,
+		hasDegree: true,
+	}
 
-	go func(arr []string) {
-		for _, color := range arr {
-			firstChannel <- color // Kanala veriyi gönderiyoruz.
-			time.Sleep(time.Second)
-		}
-	}(colors)
-
-	go func() {
-		for i := 0; i < 5; i++ {
-			data := <-firstChannel // Kanaldan alınan veri data değişkenine atanıyor.
-			fmt.Println(data)
-		}
-	}()
-	<-time.After(time.Second * 7)
+	fmt.Println(employee)
+	fmt.Println(manager)
+	fmt.Println("s")
 }
