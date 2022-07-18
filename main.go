ce6be0bf9f8ffff614aab67e10c5bc5d37734854
Yefhem <sametcelikbusiness@gmail.com>
2022-07-18 13:55:43 +0300

    main.go edited

--- main.go
+++ main.go
@@ -1,27 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
-
-	foods := map[string]interface{}{
-		"bacon": "delicious",
-		"eggs": struct {
-			source string
-			price  float64
-		}{"chicken", 1.75},
-		"steak": true,
-	}
-
-	for k, v := range foods {
-		switch c := v.(type) {
-		case string:
-			fmt.Printf("Item %q is a string, containing %q\n", k, c)
-		case float64:
-			fmt.Printf("Looks like item %q is a numberi specifically %f\n", k, c)
-		default:
-			fmt.Printf("Not sure what type item %q is, but I think it might be %T\n", k, c)
-		}
-	}
 
 }
